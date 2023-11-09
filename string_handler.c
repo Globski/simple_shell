406ef16ee9213e637e258da92d76b2f7928465a2
Emmanuel <kcemmy03@gmail.com>
2023-11-09 15:51:37 +0100

    Added a function to the string handler file

--- string_handler.c
+++ string_handler.c
@@ -5,7 +5,7 @@
  * @s: pointer to the string.
  * Return: length of the string.
  */
-int _strlen(const char *s)
+int _strlen(char *s)
 {
 	int len;
 
@@ -14,4 +14,19 @@ int _strlen(const char *s)
 		len++;
 
 	return (len)
+}
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' || *s2 != '\0')
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		else
+			return (*s1 - *s2);
+	}
+	return (0);
 }
