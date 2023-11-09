f4413c4e9bcbb2bd541c122f07448ff7d7900fc8
Globski <gloria.ogunsemore@gmail.com>
2023-11-09 07:53:58 -0800

    Add handle_env function to builtin_handler.c

---
+++ builtin_handler.c
@@ -0,0 +1,24 @@
+#include "hsh.h"
+
+/**
+ * handle_env - Prints the current environment variables.
+ * environment variables, one per line.
+ *
+ * Description: It iterates through the `environ` array and
+ * prints each variable.
+ *
+ * Return: No return value.
+ */
+int handle_env(void)
+{
+	char **env = environ;
+
+	if (!env)
+		return (-1);
+
+	while (*env != NULL)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
+}
