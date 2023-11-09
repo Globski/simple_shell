c3a6e85ef4907e17172a29b1a9a3e7f4f59ad931
Emmanuel <kcemmy03@gmail.com>
2023-11-09 15:14:49 +0100

    Added the header file

---
+++ hsh.h
@@ -0,0 +1,13 @@
+#ifndef HSH_H
+#define HSH_H
+
+/**
+ * Description - Header file containing all function prototype used in the simple
+ *		shell project.
+ */
+
+#include <stdio.h>
+
+extern char **environ;
+
+#endif /* #ifndef HSH_H */
