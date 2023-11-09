205d00e9ee36350f2786a630794350b13f74ce5a
Emmanuel <kcemmy03@gmail.com>
2023-11-09 15:35:52 +0100

    Added the string handler file

---
+++ string_handler.c
@@ -0,0 +1,17 @@
+#include "hsh.h"
+
+/**
+ * _strlen - calculates the length of a string.
+ * @s: pointer to the string.
+ * Return: length of the string.
+ */
+int _strlen(const char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len])
+		len++;
+
+	return (len)
+}
