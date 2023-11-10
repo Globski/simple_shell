9d60cf6d5bbd9f6d5fa819eea374a3dc8e86de92
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-10 14:21:27 -0800

    Update Comment for 'handle_exit' Function.

--- builtin-handler.c
+++ builtin-handler.c
@@ -19,6 +19,13 @@ void handle_env(char **args)
 /**
  * handle_exit - Handle the 'exit' command.
  * @args: An array of command arguments.
+ *
+ * Description: This function processes the 'exit' command, which can include an optional
+ * exit status. It takes an array of strings (command arguments) and
+ * checks if it's the 'exit' command or 'exit' with an optional status.
+ * If an exit status is provided, it exits the program with that status,
+ * otherwise, it exits with a status of 0. If the argument is not a valid
+ * integer or '0', it displays an error message.
  */
 void handle_exit(char **args)
 {
@@ -36,3 +43,4 @@ void handle_exit(char **args)
 	else
 		exit(0);
 }
+
