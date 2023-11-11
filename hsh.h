ddf251be68b14e225a7b799a342898e45bc0b874
Emmanuel <kcemmy03@gmail.com>
2023-11-11 06:27:04 +0100

    Added some header files

--- hsh.h
+++ hsh.h
@@ -6,10 +6,13 @@
  *               simple shell project.
  */
 
+#include <unistd.h>
 #include <string.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 extern char **environ;
 
