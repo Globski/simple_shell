d851ff33442bcf8e61c3a1623a06f657207321b4
Emmanuel <kcemmy03@gmail.com>
2023-11-15 04:11:38 +0100

    Fixed betty issues

--- builtin-handler.c
+++ builtin-handler.c
@@ -20,16 +20,17 @@ void handle_env(char **args)
  * handle_exit - Handle the 'exit' command.
  * @args: An array of command arguments.
  *
- * Description: This function processes the 'exit' command, which can include an optional
- * exit status. It takes an array of strings (command arguments) and
- * checks if it's the 'exit' command or 'exit' with an optional status.
- * If an exit status is provided, it exits the program with that status,
- * otherwise, it exits with a status of 0. If the argument is not a valid
- * integer or '0', it displays an error message.
+ * Description: This function processes the 'exit' command, which can
+ * include an optional exit status. It takes an array of strings
+ * (command arguments) and checks if it's the 'exit' command or 'exit' with
+ * an optional status. If an exit status is provided, it exits the program
+ * with the status, otherwise, it exits with a status of 0. If the argument
+ * is not a valid integer or '0', it displays an error message.
  */
 void handle_exit(char **args)
 {
 	int status;
+
 	if (args[1] != NULL)
 	{
 		status = atoi(args[1]);
@@ -48,33 +49,35 @@ void handle_exit(char **args)
  * handle_setenv - Handle the 'setenv' command.
  * @args: An array of command arguments.
  *
- * Description: This function processes the 'setenv' command, which is used to set or modify
- * environment variables. It takes an array of strings (command arguments) and
- * checks if it has the correct format. If so, it sets the specified environment
- * variable with the provided value. If the 'setenv' command is used incorrectly,
- * it displays an error message with the correct usage.
+ * Description: This function processes the 'setenv' command, which is used to
+ * set or modify environment variables. It takes an array of strings
+ * (command arguments) and checks if it has the correct format. If so, it sets
+ * the specified environment variable with the provided value. If the 'setenv'
+ * command is used incorectly, it displays an error message with the correct
+ * usage.
  */
 void handle_setenv(char **args)
 {
-    if (args[1] != NULL && args[2] != NULL)
-    {
-        if (setenv(args[1], args[2], 1) != 0)
-            perror("setenv");
-        else
-            fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
-    }
+	if (args[1] != NULL && args[2] != NULL)
+	{
+		if (setenv(args[1], args[2], 1) != 0)
+			perror("setenv");
+		else
+			fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
+	}
 }
 
 /**
  * handle_unsetenv - Handle the 'unsetenv' command.
  * @args: An array of command arguments.
  *
- * Description: This function processes the 'unsetenv' command, which is used to unset
- * environment variables. It takes an array of strings (command arguments)
- * and checks if it has the correct format. If so, it attempts to unset the
- * specified environment variable. If the variable is successfully unset,
- * it does nothing. If the variable doesn't exist or is not provided as
- * an argument, it displays an error message with the correct usage.
+ * Description: This function processes the 'unsetenv' command, which is used
+ * to unset environment variables. It takes an array of strings
+ * (command arguments) and checks if it has the correct format. If so, it
+ * attempts to unset the specified environment variable. If the variable is
+ * sucessfully unset, it does nothing. If the variable doesn't exist or is not
+ * provided as an argument, it displays an error message with the correct
+ * usage.
  */
 void handle_unsetenv(char **args)
 {
