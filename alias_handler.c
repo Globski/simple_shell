72a7608f2d957aca56cf9e642a4ffc813ccca89b
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-15 23:28:27 -0800

    Remove  handle_alias function.

--- alias_handler.c
+++ alias_handler.c
@@ -1,27 +1,6 @@
 #include "hsh.h"
 
 /**
- * handle_alias - Handles the 'alias' built-in command
- * @args: An array of command arguments, with the command as the first element
- *
- * This function implements the 'alias' command, allowing the user to manage
- * aliases. If no arguments are provided, it prints the current list of aliases
- * using the print_alias function. If the user provides proper arguments,
- * it allows adding or replacing aliases. If the second argument is ' = ', it
- * replaces an existing alias with a new value using replace_alias. Otherwise,
- * it adds a new alias using the add_alias function.
- */
-void handle_alias(char *args[])
-{
-	if (args[1] == NULL)
-		print_alias();
-	else if (args[2] != NULL && strcmp(args[2], "=") == 0)
-		replace_alias(args[1], args[3]);
-	else
-		add_alias(args[1], args[2]);
-}
-
-/**
  * add_alias - Adds a new alias or updates an existing one.
  * @name: The name of the alias to add or update.
  * @value: The value to assign to the alias.
