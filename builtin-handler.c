8467c9e400847bea5f439a56a44dd25268b998aa
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-15 19:16:09 -0800

    Add handle_exit function implementation..

--- builtin-handler.c
+++ builtin-handler.c
@@ -46,3 +46,41 @@ void handle_unsetenv(char *variable)
 		return;
 	}
 }
+
+/**
+ * handle_exit - Handles the 'exit' built-in command
+ * @status: The exit status as a string
+ *
+ * Description: This function processes the 'exit' command, allowing
+ * the user to exit the shell. If no status is provided, it exits with
+ * EXIT_SUCCESS. If a numeric status is provided, it exits with the
+ * corresponding exit code. Otherwise, it prints an error message.
+ */
+void handle_exit(char *status)
+{
+        int valid = 1;
+        size_t i;
+
+        if (status == NULL)
+                exit(EXIT_SUCCESS);
+        else
+        {
+                valid = 1;
+                for (i = 0; i < strlen(status); i++)
+                {
+                        if (!isdigit(status[i]))
+                        {
+                                valid = 0;
+                                break;
+                        }
+                }
+
+                if (valid)
+                {
+                        int exit_code = atoi(status);
+                        exit(exit_code);
+                }
+                else
+                        fprintf(stderr, "/bin/sh: 1: exit: Illegal number: %s\n", status);
+        }
+}
