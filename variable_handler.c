7d4734f650ea783b6add5c68de188268a7a662cb
Globski <gloria.ogunsemore@gmail.com>
2023-11-16 00:21:04 -0800

    Implemented the handle_variable_assignment function

--- variable_handler.c
+++ variable_handler.c
@@ -42,4 +42,45 @@ void handle_variable_replacement(char *command)
 	printf("\n");
 }
 
+/**
+ * handle_variable_assignment - Handles variable assignments in the shell.
+ * @assignment: The string containing the variable assignment.
+ *
+ * This function parses the input string containing a variable assignment,
+ * separates the variable name and value, removes leading and trailing
+ * whitespaces, and sets the environment variable accordingly
+ * using setenv. If the assignment is invalid,
+ * an error message is printed to stderr.
+ */
+void handle_variable_assignment(char *assignment)
+{
+	char *equal_sign, *variable, *value;
+	size_t value_length;
+
+	equal_sign = strchr(assignment, '=');
+	if (equal_sign == NULL || equal_sign == assignment)
+	{
+		fprintf(stderr, "Error: Invalid variable assignment: %s\n", assignment);
+		return;
+	}
+
+	*equal_sign = '\0';
+	variable = assignment;
+	value = equal_sign + 1;
+
+	while (*variable != '\0' && isspace(*variable))
+		variable++;
+
+	value_length = strlen(value);
+	while (value_length > 0 && isspace(value[value_length - 1]))
+		value_length--;
+
+	value[value_length] = '\0';
+
+	if (setenv(variable, value, 1) != 0)
+	{
+		perror("setenv");
+		return;
+	}
+}
 
