18bc60dc7517cded109ece326fb7cd6d082d7f23
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-15 22:35:56 -0800

    Implemented _getline function.

--- external_handler.c
+++ external_handler.c
@@ -45,3 +45,56 @@ void handle_comment(char *command)
 	if (comment_position != NULL)
 		*comment_position = '\0';
 }
+
+
+/**
+ * _getline - Custom getline function to read a line from standard input
+ *
+ * Return: On success, returns a pointer to the read line. On failure or
+ * end of file, returns NULL.
+ */
+char *_getline()
+{
+        static char buffer[BUFFER_SIZE];
+        static size_t buffer_index = 0, buffer_size = 0;
+        size_t end_position, line_size, read_size, i;
+        char *line;
+
+        if (buffer_index >= buffer_size)
+        {
+                read_size = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+                if (read_size == -1)
+                {
+                        perror("read");
+                        exit(EXIT_FAILURE);
+                }
+                else if (read_size == 0)
+                        return NULL;
+
+                buffer_size = (size_t)read_size;
+                buffer_index = 0;
+        }
+
+        end_position = buffer_index;
+        while (end_position < buffer_size && buffer[end_position] != '\n')
+                end_position++;
+
+        line_size = end_position - buffer_index;
+        line = (char *)malloc(line_size + 1);
+        if (!line)
+        {
+                perror("malloc");
+                exit(EXIT_FAILURE);
+        }
+        for (i = 0; i < line_size; i++)
+                line[i] = buffer[buffer_index + i];
+
+        line[line_size] = '\0';
+
+        if (end_position < buffer_size && buffer[end_position] == '\n')
+                buffer_index = end_position + 1;
+        else
+                buffer_index = buffer_size;
+
+        return line;
+}
