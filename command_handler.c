7bc563c17a346cc46ea07aa18789910301854c20
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-15 19:46:51 -0800

    Removed a line of code.

--- command_handler.c
+++ command_handler.c
@@ -1,26 +1,6 @@
 #include "hsh.h"
 
 /**
- * get_args - Function that gets the argument and tokenizes it.
- * @command: the argument.
- * @args: the buffer to store the array of strings.
- */
-void get_args(char *command, char *args[])
-{
-	int i = 0;
-	char *token;
-
-	token = strtok(command, " ");
-	while (token != NULL)
-	{
-		args[i] = token;
-		i += 1;
-		token = strtok(NULL, " ");
-	}
-	args[i] = NULL;
-}
-
-/**
  * handle_command - Parses and handles user input commands
  * @command: The input command string
  *
