33ef4cc85d20a0ca3a30d8b97737864f8a3468b7
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-17 14:10:12 -0800

    implement process_external_command function.

--- execute_handler.c
+++ execute_handler.c
@@ -1,1 +1,10 @@
 #include "hsh.h"
+
+/**
+ * process_external_command - Process an external command.
+ * @input: The command string to process.
+ */
+void process_external_command(char *input)
+{
+    execute_command(input);
+}
