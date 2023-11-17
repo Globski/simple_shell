68786c083b9e45377eeccded8e74e3935db3aba3
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-17 13:49:39 -0800

    implement tokenize_command function.

--- command_handler.c
+++ command_handler.c
@@ -52,3 +52,22 @@ void handle_command(char *command)
 		execute_command(args);
 	free_arguments(args);
 }
+
+/**
+ * tokenize_command - Tokenize the given command into an array of arguments.
+ * @command: The command string to tokenize.
+ * @args: An array to store the resulting arguments.
+ */
+void tokenize_command(char *command, char *args[])
+{
+    int i = 0;
+    char *token = _strtok(command, " ");
+
+    while (token != NULL)
+    {
+        args[i++] = token;
+        token = _strtok(NULL, " ");
+    }
+
+    args[i] = NULL;
+}
