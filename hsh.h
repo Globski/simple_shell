ba76c3762081d7b17a69bb7e5e000b8d82a9f078
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-17 13:20:33 -0800

    Add prototype for handle_unsetenv.

--- hsh.h
+++ hsh.h
@@ -49,6 +49,7 @@ char *_strchr(char *s, char c);
 
 /* builtin_handler.c */
 void handle_setenv(char *arg);
+void handle_unsetenv(char *arg);
 void handle_env(void);
 void handle_exit(char *status);
 void handle_cd(char *dir);
