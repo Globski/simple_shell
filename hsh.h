051640f2f7b4b6e5d27a5d35bde3390d32e08920
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-17 13:42:40 -0800

    Remove function prototypes in bulk.

--- hsh.h
+++ hsh.h
@@ -16,29 +16,9 @@
 #include <fcntl.h>
 
 #define MAX_INPUT_SIZE 1024
-#define MAX_ARGS 100
 #define BUFFER_SIZE 1024
-#define MAX_ALIASES 100
 
 extern char **environ;
-
-/**
- * struct Alias - Represents an alias in the shell.
- * @name: The name of the alias.
- * @value: The value associated with the alias.
- */
-typedef struct Alias
-{
-/* Structure to represent an alias */
-	char *name;
-	char *value;
-} Alias;
-
-/* Array to store aliases */
-static Alias aliases[MAX_ALIASES];
-
-/* Counter for the number of aliases */
-static int numAliases;
 
 /* string-handler.c */
 int _strlen(char *s);
@@ -54,16 +34,6 @@ void handle_exit(char *arg);
 void handle_env(void);
 void handle_cd(char *dir);
 
-/* command_handler.c */
-void handle_command(char *command);
-void free_arguments(char *args[]);
-
-/* path_handler.c */
-void handle_path(char *command);
-
-/* file_handler.c */
-void run_file_command(const char *filename);
-
 /* execute_command.c */
 void execute_command(char *args[]);
 
@@ -72,23 +42,9 @@ char *_strtok(char *str, const char *delim);
 void handle_comment(char *command);
 char *_getline();
 
-/* alias_handler.c */
-void add_alias(char *name, char *value);
-void print_alias(void);
-void cleanup_aliases(void);
-void replace_alias(char *name, char *value);
-void set_alias(char *name, char *value);
-
 /* operators_handler.c */
 void handle_command_separator(char *command);
 void handle_logical_and(char *command);
 void handle_logical_or(char *command);
-
-/* handle_alias.c */
-void handle_alias(char *args[]);
-
-/* variable_handler.c */
-void handle_variable_replacement(char *command);
-void handle_variable_assignment(char *assignment);
 
 #endif /* #ifndef HSH_H */
