31fdb491be66eb3f15daa9d5cfe9cf4b289cb056
Emmanuel <kcemmy03@gmail.com>
2023-11-17 23:39:47 +0100

    Fixed betty issues

--- execute_handler.c
+++ execute_handler.c
@@ -7,14 +7,15 @@
 void execute_command(char *command)
 {
 	char *args[MAX_ARGS];
+
 	substitute_variables(command);
 	tokenize_command(command, args);
-
 	if (args[0] != NULL)
 	{
 		if (strcmp(args[0], "cd") == 0)
 		{
 			char *dir = args[1];
+
 			handle_cd(dir);
 		}
 		else
