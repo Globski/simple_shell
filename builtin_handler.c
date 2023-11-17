a3a79f36c35e03ac5fe7ce09e2c3efaff0caccd6
Emmanuel <kcemmy03@gmail.com>
2023-11-17 22:41:45 +0100

    Implemented the handle_cd function

--- builtin_handler.c
+++ builtin_handler.c
@@ -6,18 +6,18 @@
  */
 void handle_setenv(char *arg)
 {
-    char *name = _strtok(arg, " ");
-    char *value = _strtok(NULL, " ");
+	char *name = _strtok(arg, " ");
+	char *value = _strtok(NULL, " ");
 
-    if (name != NULL && value != NULL)
-    {
-        handle_special_characters(value);
+	if (name != NULL && value != NULL)
+	{
+		handle_special_characters(value);
 
-        if (setenv(name, value, 1) != 0)
-            fprintf(stderr, "setenv: Unable to set environment variable\n");
-    }
-    else
-        fprintf(stderr, "setenv: Invalid syntax\n");
+		if (setenv(name, value, 1) != 0)
+			fprintf(stderr, "setenv: Unable to set environment variable\n");
+	}
+	else
+		fprintf(stderr, "setenv: Invalid syntax\n");
 }
 
 /**
@@ -26,15 +26,15 @@ void handle_setenv(char *arg)
  */
 void handle_unsetenv(char *arg)
 {
-    char *name = _strtok(arg, " ");
+	char *name = _strtok(arg, " ");
 
-    if (name != NULL)
-    {
-        if (unsetenv(name) != 0)
-            fprintf(stderr, "unsetenv: Unable to unset environment variable\n");
-    }
-    else
-        fprintf(stderr, "unsetenv: Invalid syntax\n");
+	if (name != NULL)
+	{
+		if (unsetenv(name) != 0)
+			fprintf(stderr, "unsetenv: Unable to unset environment variable\n");
+	}
+	else
+		fprintf(stderr, "unsetenv: Invalid syntax\n");
 }
 
 /**
@@ -43,13 +43,13 @@ void handle_unsetenv(char *arg)
  */
 void handle_exit(char *arg)
 {
-    char *endptr;
-    long exitCode = strtol(arg, &endptr, 10);
+	char *endptr;
+	long exitCode = strtol(arg, &endptr, 10);
 
-    if (*endptr == '\0')
-        exit(exitCode);
-    else
-        fprintf(stderr, "/bin/sh: 3: exit: Illegal number: %s\n", arg);
+	if (*endptr == '\0')
+		exit(exitCode);
+	else
+		fprintf(stderr, "/bin/sh: 3: exit: Illegal number: %s\n", arg);
 }
 
 
@@ -73,17 +73,12 @@ void handle_env(void)
 
 /**
  * handle_cd - Change the current working directory.
- * @dir: The target directory path.
- *
- * Change the current working directory based on the specified
- * directory path. Supports changing to the home directory, the
- * previous directory, or a specific directory.
+ * @dir: Target directory to change to, or NULL for the home directory.
  */
 void handle_cd(char *dir)
 {
-	char *home_dir = getenv("HOME"), new_dir[MAX_ARGS];
-	char *previous_dir = getenv("PWD"), current_dir[MAX_ARGS];
-	(void) numAliases, (void) aliases;
+	char *home_dir = getenv("HOME"), current_dir[MAX_ARGS];
+	char *previous_dir = getenv("PWD");
 
 	if (dir == NULL)
 	{
@@ -93,7 +88,7 @@ void handle_cd(char *dir)
 			return;
 		}
 		else
-			strncpy(new_dir, home_dir, sizeof(new_dir) - 1);
+			chdir(home_dir);
 	}
 	else if (strcmp(dir, "-") == 0)
 	{
@@ -103,18 +98,16 @@ void handle_cd(char *dir)
 			return;
 		}
 		else
-			strncpy(new_dir, previous_dir, sizeof(new_dir) - 1);
+			chdir(previous_dir);
 	}
-	else
-		strncpy(new_dir, dir, sizeof(new_dir) - 1);
+	else if (chdir(dir) != 0)
+	{
+		perror("chdir");
+		return;
+	}
 	if (getcwd(current_dir, sizeof(current_dir)) == NULL)
 	{
 		perror("getcwd");
-		return;
-	}
-	if (chdir(new_dir) != 0)
-	{
-		perror("chdir");
 		return;
 	}
 	if (setenv("PWD", current_dir, 1) != 0)
