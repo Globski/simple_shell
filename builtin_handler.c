5984511cf7e540bf73f69a275fb0caf18ce3305b
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-17 13:23:08 -0800

    Remove deprecated function handle_exit from buiilt_handler.c.

--- builtin_handler.c
+++ builtin_handler.c
@@ -36,44 +36,6 @@ void handle_unsetenv(char *arg)
     else
         fprintf(stderr, "unsetenv: Invalid syntax\n");
 }
-/**
- * handle_exit - Handles the 'exit' built-in command
- * @status: The exit status as a string
- *
- * Description: This function processes the 'exit' command, allowing
- * the user to exit the shell. If no status is provided, it exits with
- * EXIT_SUCCESS. If a numeric status is provided, it exits with the
- * corresponding exit code. Otherwise, it prints an error message.
- */
-void handle_exit(char *status)
-{
-	int valid = 1, exit_code;
-	size_t i;
-	(void) numAliases, (void) aliases;
-
-	if (status == NULL)
-		exit(EXIT_SUCCESS);
-	else
-	{
-		valid = 1;
-		for (i = 0; i < strlen(status); i++)
-		{
-			if (!isdigit(status[i]))
-			{
-				valid = 0;
-				break;
-			}
-		}
-
-		if (valid)
-		{
-			exit_code = atoi(status);
-			exit(exit_code);
-		}
-		else
-			fprintf(stderr, "/bin/sh: 1: exit: Illegal number: %s\n", status);
-	}
-}
 
 /**
  * handle_env - Displays the current environment variables
