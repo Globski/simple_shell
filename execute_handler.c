578c3b0457c94506a8d8786529c11c7ae2afb49a
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-17 14:18:53 -0800

    Implement execute_command function.

--- execute_handler.c
+++ execute_handler.c
@@ -1,6 +1,28 @@
 #include "hsh.h"
 
 /**
+ * execute_command - Execute the given command.
+ * @command: The command string to execute.
+ */
+void execute_command(char *command)
+{
+    char *args[MAX_ARGS];
+    substitute_variables(command);
+    tokenize_command(command, args);
+
+    if (args[0] != NULL)
+    {
+        if (strcmp(args[0], "cd") == 0)
+        {
+            char *dir = args[1];
+            handle_cd(dir);
+        }
+        else
+            execute_external_command(args);
+    }
+}
+
+/**
  * execute_external_command - Execute an external command with arguments.
  * @args: Array containing the command and its arguments.
  */
