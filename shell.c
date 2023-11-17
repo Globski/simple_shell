f170f0802b29537d6729c9e8e851dac02561cba0
Emmanuel <kcemmy03@gmail.com>
2023-11-17 23:40:10 +0100

    Fixed betty issues

--- shell.c
+++ shell.c
@@ -1,6 +1,6 @@
 #include "hsh.h"
 
-/*
+/**
  * main - Entry point of the program
  * @argc: Number of command-line arguments
  * @argv: Array of command-line argument strings
@@ -8,26 +8,28 @@
  * Description: Sets up signal handlers, saves original terminal settings,
  * and runs interactive or non-interactive mode based on the number of
  * command-line arguments.
+ *
+ * Return: 0 if sucdessful.
  */
 int main(int argc, char **argv)
 {
-    signal(SIGINT, handle_signals);
-    signal(SIGTSTP, handle_signals);
+	signal(SIGINT, handle_signals);
+	signal(SIGTSTP, handle_signals);
 
-    tcgetattr(STDIN_FILENO, &orig_termios);
+	tcgetattr(STDIN_FILENO, &orig_termios);
 
-    if (argc == 1)
-        run_interactive_mode();
-    else if (argc == 2)
-        run_non_interactive_mode();
-    else
-    {
-        fprintf(stderr, "Usage: %s [script]\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+	if (argc == 1)
+		run_interactive_mode();
+	else if (argc == 2)
+		run_non_interactive_mode();
+	else
+	{
+		fprintf(stderr, "Usage: %s [script]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-    cleanup();
+	cleanup();
 
-    return (0);
+	return (0);
 }
 
